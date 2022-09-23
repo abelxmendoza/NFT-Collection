3a600442536dee2739b09da191afd8fb6241a92c
Abel Mendoza <abelxmendoza@gmail.com>
2022-09-23 02:33:58 -0700

    first push

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xAe94b177bC93C3832f9ABE67B874245E3bb44dB0";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
